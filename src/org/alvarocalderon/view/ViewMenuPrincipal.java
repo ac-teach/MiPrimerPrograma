bec9e1f0302c0086515885fc93b54f7bc8da5f52
Alvaro Calderon <alvarocalderon@kinal.edu.gt>
2026-04-09 11:53:14 -0600

    001 Se agregaron las opciones al menu principal

--- src/org/alvarocalderon/view/ViewMenuPrincipal.java
+++ src/org/alvarocalderon/view/ViewMenuPrincipal.java
@@ -10,6 +10,10 @@ public class ViewMenuPrincipal {
         //local
         while (opcionMenu == 0){
             System.out.println("Binevenido, Seleccione una opción:");
+            System.out.println("1. Operaciones matemáticas");
+            System.out.println("2. Operaciones de texto");
+            System.out.println("3. Operaciones de lógica");
+            System.out.println("4. Salir del sistema");
             opcionMenu = Integer.parseInt(leer.nextLine());
 
         }
